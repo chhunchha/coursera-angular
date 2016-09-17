60bd467a9f4e485fa822339f1d0bba7bdc92af99
Sumant Chhunchha <mr.chhunchha@gmail.com>
2016-09-17 15:15:51 -0700

    assignment 1

---
+++ module1_solution/app.js
@@ -0,0 +1,47 @@
+angular.module("LunchCheck", [])
+.controller("LunchCheckController", LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+	var vm = this;
+	vm.lunch = "";
+	vm.message = "";
+	vm.good_flag = undefined;
+
+	function drop_empty_items(lunch_items) {
+		var no_of_items = lunch_items.length;
+		for(var i=0 ; i < no_of_items; i++) {
+			if(lunch_items[i].trim().length == 0)
+			{
+				lunch_items.splice(i,1);
+				i--;
+				no_of_items--;
+			}
+		}
+	}
+
+	vm.checkLunch = function() {
+		var lunch_items = vm.lunch.split(',');
+		// console.log(lunch_items);
+		drop_empty_items(lunch_items);
+		// console.log(lunch_items);
+		if(lunch_items.length == 0) {
+			vm.message = "Please enter data first";
+			vm.good_flag = false;
+		}
+		else if(lunch_items.length <= 3 ) {
+			vm.message = "Enjoy!";
+			vm.good_flag = true;
+		} else {
+			vm.message = "Too much!";
+			vm.good_flag = true;
+		}
+	};
+
+	vm.clearMessage = function() {
+		vm.message = "";
+		vm.good_flag = undefined;
+	};
+
+
+}
